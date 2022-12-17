849c56617238501750e759fdb05429a11bbdc072
GeorgeBahaa <georgebibo77@gmail.com>
2022-12-17 09:32:44 +0200

    push

--- DS_Project/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ds_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ds_project to javafx.fxml;
-    exports com.example.ds_project;
-}
