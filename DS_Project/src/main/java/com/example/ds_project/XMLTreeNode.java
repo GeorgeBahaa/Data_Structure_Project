f440a4fd53346be4c5e6b9646d6599dd7d6f03c7
OmarTantawy9 <omartantawy9@yahoo.com>
2022-12-17 18:38:28 +0200

    XMLTreeNode Class
    Co-Authored-By: KarineGeorge <88349234+KarineGeorge@users.noreply.github.com>

---
+++ DS_Project/src/main/java/com/example/ds_project/XMLTreeNode.java
@@ -0,0 +1,47 @@
+package com.example.ds_project;
+
+import java.util.ArrayList;
+
+public class XMLTreeNode {
+
+    private XMLTreeNode parent;
+    private String value;
+    private ArrayList<XMLTreeNode> children = new ArrayList<>();
+
+    public XMLTreeNode(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getParent() {
+        if(parent != null)
+            return parent.getValue();
+        return null;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setParent(XMLTreeNode parent) {
+        this.parent = parent;
+    }
+
+
+    public ArrayList<XMLTreeNode> getChildren() {
+        return children;
+    }
+
+    public void addChild(XMLTreeNode node) {
+        node.setParent(this);
+        this.children.add(node);
+    }
+
+    public XMLTreeNode removeLastChild() {
+        return this.children.remove(children.size()-1);
+    }
+
+}
