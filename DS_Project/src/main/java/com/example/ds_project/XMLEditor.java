cad3272323033c0f54f19aa5f6572a140ba42c2c
GeorgeBahaa <georgebibo77@gmail.com>
2022-12-17 09:40:13 +0200

    push

--- DS_Project/src/main/java/com/example/ds_project/XMLEditor.java
+++ DS_Project/src/main/java/com/example/ds_project/XMLEditor.java
@@ -1,14 +1,23 @@
 package com.example.ds_project;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class XMLEditor {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class XMLEditor extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(XMLEditor.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
