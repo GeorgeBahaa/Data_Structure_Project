ce7f114462d37678b46333234db4c7eb91f9af45
GeorgeBahaa <georgebibo77@gmail.com>
2022-12-19 14:26:55 +0200

    push

--- XMLProject/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.xmlproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.xmlproject to javafx.fxml;
-    exports com.example.xmlproject;
-}
