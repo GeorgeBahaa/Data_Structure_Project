becdb41f46ddd39c4c441a4d2644d16817c96154
GeorgeBahaa <georgebibo77@gmail.com>
2023-02-01 00:18:59 +0200

    push functions

--- DS_Project/src/main/java/com/example/ds_project/XMLGraph.java
+++ DS_Project/src/main/java/com/example/ds_project/XMLGraph.java
@@ -8,6 +8,7 @@ public class XMLGraph {
 
     public static ArrayList<ArrayList <Integer> >xmlGraphNodes ;
     private static ArrayList<ArrayList <Integer> > xmlGraphFriends ;
+    public static ArrayList<Integer> mutualFriends = new ArrayList<Integer>();
     static String id;
     static  PriorityQueue<Pair<Integer, Integer> > pq = new PriorityQueue<>((a, b) -> b.getKey() - a.getKey());
     private static ArrayList<Integer> inputArray;
@@ -97,7 +98,55 @@ public class XMLGraph {
         System.out.println("the Infuencer has ID "+pq.peek().getValue()+" and has "+ pq.peek().getKey()+" followers");
     }
 
+    public static void suggestFollowers(int id){
+        ArrayList<Integer> followrsOfFollowers = new ArrayList<>();
+        ArrayList<Integer> suggestions = new ArrayList<>();
+        for(int i=0 ; i<xmlGraphNodes.get(id).size() ; i++) {
+            followrsOfFollowers.addAll(xmlGraphNodes.get(xmlGraphNodes.get(id).get(i)));
+        }
 
+        inputArray=followrsOfFollowers;
+        sortGivenArray();
+
+        for (int i=1 ; i< followrsOfFollowers.size() ; i++){
+            if(followrsOfFollowers.get(i-1)==followrsOfFollowers.get(i)){
+                followrsOfFollowers.remove(i);
+            }
+        }
+
+        for(int i=0;i<followrsOfFollowers.size();i++){
+            if(binarySearch(xmlGraphFriends.get(id),followrsOfFollowers.get(i))==0){
+                if(id==followrsOfFollowers.get(i)) continue;
+                else suggestions.add(followrsOfFollowers.get(i));
+            }
+
+        }
+        System.out.println(suggestions);
+    }
+
+    public static void mutualfriend(int ID1,int ID2){
+        for(int i=0;i<xmlGraphNodes.get(ID1).size();i++){
+            if (binarySearch(xmlGraphNodes.get(ID2),xmlGraphNodes.get(ID1).get(i))==1)
+                mutualFriends.add(xmlGraphNodes.get(ID1).get(i));
+        }
+        System.out.println(mutualFriends);
+    }
+
+    private static int binarySearch(ArrayList<Integer> arr, int x)
+    {
+        int left = 0, right = arr.size() - 1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (arr.get(mid) == x)
+                return 1;
+            if (arr.get(mid) < x)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return 0;
+    }
 
     private static void merge(int startIndex,int midIndex,int endIndex){
 
